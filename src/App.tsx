e33c9a923fa02613874859c04c9685163b31dcaa
Pavel <p-olhovoi@bk.ru>
2022-08-20 23:30:09 +0200

    refactoring: Combine categories and products components

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,19 @@
+import Categories from './components/categories/Categories';
 import './styles/App.css'
-import AllProdacts from './pages/AllProducts';
-import { useActions } from './hooks/useAction';
-import { useEffect, useState } from 'react';
-import { connect, ConnectedProps, useSelector } from 'react-redux';
-import { State } from './store';
 
-interface Props extends PropsFromRedux {
- 
-}
 
-const App: React.FC<Props>= ({categories}) => {
-  const testim = "electronics"
-  const {categoriesActions} = useActions()
-  const [category, setCategory] = useState('')
-  
-  useEffect(()=> {
-    categoriesActions()
-  }, [])
 
-  console.log(categories)
-  
+const App = () => {
 
   return (
     <div className="App">
-        <h1>Empty Project</h1>
-        <div>
-          <h2>Categories</h2>
-          <div>
-          {
-            categories.map( (cat, index) => <div 
-            key={index}
-            onClick={()=> setCategory(cat)}
-            >
-              {cat}
-
-            </div>)
-          }
-          </div>
-        </div>
-        <AllProdacts category={category}/>   
+        <h1>Store some</h1>
+        <Categories />  
     </div>
   );
 }
 
-const mapStateToProps = (state: State) => ({
-  categories: state.categories.categories
-})
-
-const connector = connect(mapStateToProps)
-type PropsFromRedux = ConnectedProps<typeof connector>
-
-
-export default connector(App)
+export default App
 
 
 
--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,12 +1,47 @@
 import React, {FC} from "react"
-import AllProducts from "../../pages/AllProducts"
+import { connect, ConnectedProps, useSelector } from 'react-redux';
+import { State } from "../../store";
+import { useActions } from '../../hooks/useAction'
+import { useEffect, useState } from 'react';
+import AllProducts from "../../pages/AllProducts";
 
-const Categories = () => {
+interface Props extends PropsFromRedux {
+ 
+}
+const Categories: React.FC<Props> = ({categories}) => {
+    const {categoriesActions} = useActions()
+    const [category, setCategory] = useState('')
+    
+    useEffect(()=> {
+      categoriesActions()
+    }, [])
     return (
         <div className="category">
-            Category
+            <div>
+          <h2>Categories</h2>
+          <div>
+          {
+            categories.map( (cat, index) => <div 
+            key={index}
+            onClick={()=> setCategory(cat)}
+            >
+              {cat}
+
+            </div>)
+          }
+          </div>
+        </div>
+        <AllProducts category={category} />
         </div>
     )
 }
 
-export default Categories
+const mapStateToProps = (state: State) => ({
+    categories: state.categories.categories
+})
+
+const connector = connect(mapStateToProps)
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+
+export default connector(Categories)
